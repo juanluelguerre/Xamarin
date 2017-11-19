975d1b74f785a1472780e0617d64c145ff001ba7
Juan Luis Guerrero Minero <jlguerrero@gmail.com>
2017-11-19 20:40:10 +0100

    Initial repository for Simple XAE (Xamarin Azure Explorer)

---
+++ XAE/XAE/XAE.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace XAE.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new XAE.App());
+        }
+    }
+}
